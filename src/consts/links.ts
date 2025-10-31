1a42f6db4c173cbd18e69001c7566a86db26ffd1
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-10-31 15:56:13 -0600

    Update documentation links to versioned paths
    Changed 'getStarted' and 'docs' links to point to version 8.5.0 documentation. Added new links for 'releaseNotes' and 'migrationGuide' for version 8.5.0.

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,6 +1,8 @@
 export const LINKS = {
 	home: '/*',
 	download: '/download',
-	getStarted: '/get-started',
-	docs: '/docs',
+	getStarted: '/docs/8_5_0/get-started',
+	docs: '/docs/8_5_0',
+	releaseNotes: '/docs/8_5_0/php-8-5',
+	migrationGuide: '/docs/8_5_0/migration-guide'
 }
