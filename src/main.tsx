e20989e5a91099484061863221b76af3b7850eac
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-10-31 15:57:21 -0600

    Add routes for release notes and migration guides
    Added new routes for LatestReleasePage and MigrationGuidesPage, and updated the get started route to use LINKS.docs. This enhances navigation to release notes and migration guides pages.

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing.tsx";
 import GetStartedPage from "./pages/get-started.tsx";
 import { LINKS } from "./consts/links.ts";
+import LatestReleasePage from "./pages/latest-release.tsx";
+import MigrationGuidesPage from "./pages/migration-guides.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/phpnewdocs">
       <Routes>
         <Route index path={LINKS.home} element={<Landing />} />
-        <Route path={LINKS.getStarted} element={<GetStartedPage />} />
+        <Route path={LINKS.docs} element={<GetStartedPage />} />
+        <Route path={LINKS.releaseNotes} element={<LatestReleasePage />} />
+        <Route path={LINKS.migrationGuide} element={<MigrationGuidesPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
