faca69ac0738e8ce49fd2c6a69beb41ac9c59ece
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-10-30 20:36:14 -0600

    Remove unused App import from main.tsx
    The App component import was removed from main.tsx as it is not used in this file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing.tsx";
 
