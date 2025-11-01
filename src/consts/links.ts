782aa102c4d311699c727724579ef3ec54418fa8
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-10-31 19:20:40 -0600

    Update Downloads link

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
 	home: '/*',
-	download: '/download',
+	download: '/docs/8_5_0/downloads',
 	getStarted: '/docs/8_5_0/get-started',
 	docs: '/docs/8_5_0',
 	releaseNotes: '/docs/8_5_0/php-8-5',
